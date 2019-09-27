b9c1940df47b8d0b8e4fe8da4a177adb4ce40bf3
mickykebe <mickykebe@gmail.com>
2019-09-27 14:36:13 +0300

    try trust proxy 2

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const ***REMOVED*** isProduction ***REMOVED*** = require("./utils");
 
 const app = express();
 if (isProduction) ***REMOVED***
-  app.set("trust proxy", 1);
+  app.set("trust proxy", 2);
 ***REMOVED***
 app.use("/api", routes);
 
