bda18c2781f49dffae4b2fa164c282103043af6d
mickykebe <mickykebe@gmail.com>
2019-09-27 13:27:09 +0300

    trust proxy to fix ssl session

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const routes = require("./routes/index");
+const { isProduction } = require("./utils");
 
 const app = express();
+if (isProduction) {
+  app.set("trust proxy", 1);
+}
 app.use("/api", routes);
 
 module.exports = app;
