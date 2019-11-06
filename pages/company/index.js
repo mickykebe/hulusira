9d7124faf1e9d065d258c07018de11b02a88a3df
mickykebe <mickykebe@gmail.com>
2019-11-06 17:42:48 +0300

    remove company jobs on company delete

--- pages/company/index.js
+++ pages/company/index.js
@@ -112,8 +112,7 @@ function Companies({ user, companies }) {
         <DialogTitle>Delete this company?</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Deleting this company will dissociate it from all jobs created under
-            it.
+            Deleting this company will remove all jobs posted under it.
           </DialogContentText>
         </DialogContent>
         <DialogActions>
