cd0aa70a79730143510f7694528c20132be5862a
mickykebe <mickykebe@gmail.com>
2019-11-10 10:34:03 +0300

    add ads.txt

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withCSS = require("@zeit/next-css");
 module.exports = withCSS(***REMOVED***
   env: ***REMOVED***
     ROOT_URL: process.env.ROOT_URL
-  ***REMOVED***
+  ***REMOVED***,
+  experimental: ***REMOVED*** publicDirectory: true ***REMOVED***
 ***REMOVED***);
