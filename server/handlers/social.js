1cd2cd049fc0f9f8102ba19b1f9fbae48b38368a
mickykebe <mickykebe@gmail.com>
2019-11-18 19:35:07 +0300

    remove logs from handlers/social

--- server/handlers/social.js
+++ server/handlers/social.js
@@ -159,7 +159,6 @@ exports.postJobCloseToSocialMedia = async function(jobData) {
   }
   const { telegramMessages, telegramMessageId, facebookPostId } = messageIds;
 
-  console.log({ telegramMessages, telegramMessageId, facebookPostId });
   if (!!telegramMessages) {
     try {
       await Promise.all(
@@ -177,8 +176,6 @@ exports.postJobCloseToSocialMedia = async function(jobData) {
       );
     }
   }
-
-  console.log({ channelUserName: process.env.TELEGRAM_CHANNEL_USERNAME });
 
   if (!!telegramMessageId) {
     try {
