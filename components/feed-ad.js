1a5cd01d0932d02475901926e0673892d13cf612
mickykebe <mickykebe@gmail.com>
2019-11-19 15:36:37 +0300

    feed ad fixed height 120

--- components/feed-ad.js
+++ components/feed-ad.js
@@ -1,5 +1,5 @@
 import GoogleAd from "./google-ad";
 
 export default function FeedAd() ***REMOVED***
-  return <GoogleAd dataAdLayouKey="-ha-6+1u-6q+8y" dataAdSlot="8888209775" style=***REMOVED******REMOVED*** maxHeight: 150 ***REMOVED******REMOVED*** />;
+  return <GoogleAd dataAdLayouKey="-ha-6+1u-6q+8y" dataAdSlot="8888209775" style=***REMOVED******REMOVED*** height: 120 ***REMOVED******REMOVED*** />;
 ***REMOVED***
