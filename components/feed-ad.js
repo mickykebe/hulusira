421c3763bba4055fadaa56e3f5c99443b56c6b43
mickykebe <mickykebe@gmail.com>
2019-11-19 15:18:55 +0300

    inline style max height on feed ad

--- components/feed-ad.js
+++ components/feed-ad.js
@@ -1,13 +1,6 @@
 import GoogleAd from "./google-ad";
-import ***REMOVED*** makeStyles ***REMOVED*** from "@material-ui/core";
-
-const useStyles = makeStyles(theme => (***REMOVED***
-  root: ***REMOVED***
-    maxHeight: 150,
-  ***REMOVED***,
-***REMOVED***));
 
 export default function FeedAd() ***REMOVED***
   const classes = useStyles();
-  return <GoogleAd dataAdLayouKey="-ha-6+1u-6q+8y" dataAdSlot="8888209775" className=***REMOVED***classes.root***REMOVED*** />;
+  return <GoogleAd dataAdLayouKey="-ha-6+1u-6q+8y" dataAdSlot="8888209775" style=***REMOVED******REMOVED*** maxHeight: 150 ***REMOVED******REMOVED*** />;
 ***REMOVED***
