4e61e49d3308fe346b17ca1d0ecd6f74d624a0d8
mickykebe <mickykebe@gmail.com>
2019-11-19 15:27:34 +0300

    bug fix feed ad

--- components/feed-ad.js
+++ components/feed-ad.js
@@ -1,6 +1,5 @@
 import GoogleAd from "./google-ad";
 
 export default function FeedAd() ***REMOVED***
-  const classes = useStyles();
   return <GoogleAd dataAdLayouKey="-ha-6+1u-6q+8y" dataAdSlot="8888209775" style=***REMOVED******REMOVED*** maxHeight: 150 ***REMOVED******REMOVED*** />;
 ***REMOVED***
