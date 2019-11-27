8f05474572a34748a7faf4b5a3835c97a1c9f2d7
mickykebe <mickykebe@gmail.com>
2019-11-27 10:52:32 +0300

    remove port from next config var

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(***REMOVED***
   env: ***REMOVED***
-    PORT: process.env.PORT,
     TELEGRAM_BOT_NAME: process.env.TELEGRAM_BOT_NAME,
     ROOT_URL: process.env.ROOT_URL
   ***REMOVED***,
