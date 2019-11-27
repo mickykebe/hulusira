8c4aa7b5d528a11a70bfc9ff893616eacf616b09
mickykebe <mickykebe@gmail.com>
2019-11-27 10:35:08 +0300

    fix: port env vars in next config telegram bot name for tel login comes from env var

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   env: {
+    PORT: process.env.PORT,
+    TELEGRAM_BOT_NAME: process.env.TELEGRAM_BOT_NAME,
     ROOT_URL: process.env.ROOT_URL
   },
   experimental: { publicDirectory: true }
