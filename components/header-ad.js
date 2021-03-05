8c8df13f46f5935b07b9756aa48c8e95ae1beef4
Michael K Tedla <mickykebe@gmail.com>
2021-03-05 10:33:48 +0300

    set header ad height to 120

--- components/header-ad.js
+++ components/header-ad.js
@@ -3,7 +3,7 @@ import GoogleAd from "./google-ad";
 export default function HeaderAd({ className }) {
   return (
     <GoogleAd
-      style={{ height: 90 }}
+      style={{ height: 120 }}
       className={className}
       dataAdFormat="horizontal"
       dataAdSlot="7862886800"
