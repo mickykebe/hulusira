1471261829070585789786ce5de0a34ef4dbd7a6
Michael K Tedla <mickykebe@gmail.com>
2021-03-28 16:48:12 +0300

    center ad by default

--- components/google-ad.js
+++ components/google-ad.js
@@ -17,7 +17,11 @@ export default function GoogleAd({
   }, []);
   if (process.env.NODE_ENV === "production") {
     return (
-      <Box minWidth={250} className={className}>
+      <Box
+        minWidth={250}
+        display="flex"
+        justifyContent="center"
+        className={className}>
         <ins
           className="adsbygoogle"
           style={{ display: "block", minWidth: 250, ...style }}
